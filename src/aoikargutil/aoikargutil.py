2176c69d5cfc8af9586bcfb19161699cba1b6764
Aoi.Kuiyuyou <aoi.kuiyuyou@gmail.com>
2014-12-22 17:37:39 +0800

    version 0.1.1

--- src/aoikargutil/aoikargutil.py
+++ src/aoikargutil/aoikargutil.py
@@ -5,7 +5,7 @@ import re
 import sys
 
 #/
-__version__ = '0.1'
+__version__ = '0.1.1'
 
 #/
 def str_nonempty(txt):
@@ -35,6 +35,24 @@ def bool_0or1(txt):
         raise ArgumentTypeError('|%s| is not 0 or 1.' % txt)
 
 #/
+def float_lt0(txt):
+    try:
+        val = float(txt)
+        assert val < 0
+    except Exception:
+        raise ArgumentTypeError('|%s| is not a negative number.' % txt)
+    return val
+
+#/
+def float_le0(txt):
+    try:
+        val = float(txt)
+        assert val <= 0
+    except Exception:
+        raise ArgumentTypeError('|%s| is not zero or a negative number.' % txt)
+    return val
+
+#/
 def float_gt0(txt):
     try:
         val = float(txt)
@@ -50,6 +68,24 @@ def float_ge0(txt):
         assert val >= 0
     except Exception:
         raise ArgumentTypeError('|%s| is not zero or a positive number.' % txt)
+    return val
+
+#/
+def int_lt0(txt):
+    try:
+        val = int(txt)
+        assert val < 0
+    except Exception:
+        raise ArgumentTypeError('|%s| is not a negative integer.' % txt)
+    return val
+
+#/
+def int_le0(txt):
+    try:
+        val = int(txt)
+        assert val <= 0
+    except Exception:
+        raise ArgumentTypeError('|%s| is not zero or a negative integer.' % txt)
     return val
 
 #/
@@ -133,19 +169,78 @@ def ensure_req_pairs(parser, pairs, args=None):
     #/
     for pair in pairs:
         #/
-        arg_a, arg_b = pair
-
-        arg_a_rec = re.compile('^%s($|=|[0-9])' % arg_a)
-
-        arg_b_rec = re.compile('^%s($|=|[0-9])' % arg_b)
-
+        arg_a_spec, arg_b_spec = pair
+        
         #/
-        if any(map(lambda x: bool(arg_a_rec.search(x)), args))\
-        and not any(map(lambda x: bool(arg_b_rec.search(x)), args)):
+        if isinstance(arg_a_spec, (list, tuple)):
+            arg_a_s = arg_a_spec
+        else:
+            arg_a_s = [arg_a_spec]
+            
+        #/
+        for arg_a in arg_a_s:
             #/
-            msg = 'argument %s: requires argument %s' % (arg_a, arg_b)
-
-            parser.error(msg)
-            ## raise error
-
-            assert False
+            arg_a_rec = re.compile('^%s($|=|[0-9])' % arg_a)
+            
+            #/
+            arg_a_exists = any(bool(arg_a_rec.search(arg)) for arg in args)
+            
+            #/
+            if arg_a_exists:
+                #/
+                if isinstance(arg_b_spec, (list, tuple)):
+                    #/
+                    arg_b_s = arg_b_spec
+                else:
+                    #/
+                    arg_b_s = [arg_b_spec]
+                
+                #/
+                arg_b_rec_s = [re.compile('^%s($|=|[0-9])' % arg_b) for arg_b in arg_b_s]
+                
+                #/
+                if isinstance(arg_b_spec, list):
+                    req_all_arg_bs = True
+                else:
+                    req_all_arg_bs = False
+                    
+                #/
+                arg_b_exists = False
+                
+                for arg_b_rec in arg_b_rec_s:
+                    #/
+                    arg_b_exists = any(bool(arg_b_rec.search(arg)) for arg in args)
+                    
+                    #/
+                    if arg_b_exists:
+                        if not req_all_arg_bs:
+                            break
+                    else:
+                        if req_all_arg_bs:
+                            break
+                
+                #/
+                if not arg_b_exists:
+                    #/
+                    if isinstance(arg_b_spec, list):
+                        #/
+                        msg = 'argument %s: requires all of the arguments %s' % (arg_a, ', '.join(arg_b_spec))
+            
+                        parser.error(msg)
+                        ## raise error
+                    #/
+                    elif isinstance(arg_b_spec, tuple):
+                        #/
+                        msg = 'argument %s: requires one of the arguments %s' % (arg_a, ', '.join(arg_b_spec))
+            
+                        parser.error(msg)
+                        ## raise error
+                    else:
+                        #/
+                        msg = 'argument %s: requires argument %s' % (arg_a, arg_b_spec)
+            
+                        parser.error(msg)
+                        ## raise error
+                    
+                    #/
+                    assert False
